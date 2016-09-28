caa7b766bb975fd290332426bc5804bc89861e28
Dan Falcone <danfalcone@gmail.com>
2016-09-28 02:02:51 -0400

    Add support for postgres 9.1 and older

--- lib/pgreset.rb
+++ lib/pgreset.rb
@@ -5,7 +5,13 @@ module ActiveRecord
     class PostgreSQLDatabaseTasks
       def drop
         establish_master_connection
-        connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{configuration['database']}' AND state='idle';"
+        
+        pid_column = 'pid'       # Postgresql 9.2 and newer
+        if 0 == connection.select_all("SELECT column_name FROM information_schema.columns WHERE table_name = 'pg_stat_activity' AND column_name = 'pid';").count
+          pid_column = 'procpid' # Postgresql 9.1 and older
+        end
+
+        connection.select_all "SELECT pg_terminate_backend(pg_stat_activity.#{pid_column}) FROM pg_stat_activity WHERE datname='#{configuration['database']}' AND #{pid_column} <> pg_backend_pid();"
         connection.drop_database configuration['database']
       end
     end
